44fb50c2e279a86ce77bebf12d11a248bb480bd8
Jeb Atkinson <jxa2576@g.rit.edu>
2018-09-17 16:14:22 -0400

    User input, variable width

--- SlidingPuzzle/SlidingPuzzle/SlidingPuzzle.cpp
+++ SlidingPuzzle/SlidingPuzzle/SlidingPuzzle.cpp
@@ -4,13 +4,13 @@
 
 using namespace std;
 
-void printBoard(int** b) {
-	cout << "--------------" << endl;
-	for (size_t col = 0; col < 4; col++)
+void printBoard(int size, int** b) {
+	cout << " " << endl;
+	for (size_t col = 0; col < size; col++)
 	{
 		cout << "|";
 
-		for (size_t row = 0; row < 4; row++)
+		for (size_t row = 0; row < size; row++)
 		{
 			if (b[col][row] != -1) {
 				cout << b[col][row];
@@ -26,17 +26,17 @@ void printBoard(int** b) {
 
 		cout << "|" << endl;
 	}
-	cout << "--------------" << endl;
+	cout << " " << endl;
 }
 
-void shuffleBoard(int** b) {
+void shuffleBoard(int shuffleAmount, int size, int** b) {
 	cout << "SHUFFLE" << endl;
 	int shuffleCount = 0;
 	srand(time(NULL));
-	while (shuffleCount < 15) {
-		for (size_t col = 0; col < 4; col++)
+	while (shuffleCount < shuffleAmount) {
+		for (size_t col = 0; col < size; col++)
 		{
-			for (size_t row = 0; row < 4; row++)
+			for (size_t row = 0; row < size; row++)
 			{
 				if (b[col][row] == 0) {
 					int randDir = rand() % 4;
@@ -57,14 +57,14 @@ void shuffleBoard(int** b) {
 						}
 						break;
 					case 2:
-						if (col + 1 != 4) {
+						if (col + 1 != size) {
 							cout << "DOWN" << endl;
 							b[col][row] = b[col + 1][row];
 							b[col + 1][row] = 0;
 						}
 						break;
 					case 3:
-						if (row + 1 != 4) {
+						if (row + 1 != size) {
 							cout << "RIGHT" << endl;
 							b[col][row] = b[col][row + 1];
 							b[col][row + 1] = 0;
@@ -78,20 +78,19 @@ void shuffleBoard(int** b) {
 	}
 }
 
-bool solveBoard(int** b) {
-	int solution[16] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0};
+bool solveBoard(int size, int** b, int** solvedB) {
 
 	int solveCount = 0;
 	bool solved = false;
-	int colPos = 3;
-	int rowPos = 3;
+	int colPos = size - 1;
+	int rowPos = size - 1;
 	int moves[4] = { 0,0,0,0 };
 
-	for (size_t col = 0; col < 4; col++)
+	for (size_t col = 0; col < size; col++)
 	{
-		for (size_t row = 0; row < 4; row++)
+		for (size_t row = 0; row < size; row++)
 		{
-			if (b[col][row] == solution[4 * col + row]) {
+			if (b[col][row] == solvedB[col][row]) {
 				solveCount++;
 			}
 			else if(b[col][row] == 0){
@@ -99,7 +98,10 @@ bool solveBoard(int** b) {
 				rowPos = row;
 				if (col - 1 != -1) {
 					//cout << "UP" << endl;
-					moves[0] = abs(b[col - 1][row] - solution[4 * (col) + row]);
+					moves[0] = abs(b[col - 1][row] - solvedB[col][row]);
+					if (b[col - 1][row] == solvedB[col - 1][row]) {
+						moves[0] = -1;
+					}
 				}
 				else {
 					moves[0] = -1;
@@ -107,23 +109,32 @@ bool solveBoard(int** b) {
 
 				if (row - 1 != -1) {
 					//cout << "LEFT" << endl;
-					moves[1] = abs(b[col][row - 1] - solution[4 * col + (row)]);
+					moves[1] = abs(b[col][row - 1] - solvedB[col][row]);
+					if (b[col][row - 1] == solvedB[col][row - 1]) {
+						moves[1] = -1;
+					}
 				}
 				else {
 					moves[1] = -1;
 				}
 
-				if (col + 1 != 4) {
+				if (col + 1 != size) {
 					//cout << "DOWN" << endl;
-					moves[2] = abs(b[col + 1][row] - solution[4 * (col) + row]);
+					moves[2] = abs(b[col + 1][row] - solvedB[col][row]);
+					if (b[col + 1][row] == solvedB[col + 1][row]) {
+						moves[2] = -1;
+					}
 				}
 				else {
 					moves[2] = -1;
 				}
 
-				if (row + 1 != 4) {
+				if (row + 1 != size) {
 					//cout << "RIGHT" << endl;
-					moves[3] = abs(b[col][row + 1] - solution[4 * col + (row)]);
+					moves[3] = abs(b[col][row + 1] - solvedB[col][row]);
+					if (b[col][row + 1] == solvedB[col][row + 1]) {
+						moves[3] = -1;
+					}
 				}
 				else {
 					moves[3] = -1;
@@ -131,7 +142,7 @@ bool solveBoard(int** b) {
 			}
 		}
 	}
-	if (solveCount == 16) {
+	if (solveCount == size * size) {
 		solved = true;
 	}
 	else {
@@ -180,53 +191,88 @@ bool solveBoard(int** b) {
 	return solved;
 }
 
-bool recursiveSolve(int** b, int depth) {
-	depth++;
+bool recursiveSolve(int** b) {
 	//check if solved
 	//foreach possible move, move, call function again, use for loop
+	//1 2,4 3,5 6,8 7,13 9, 14 10, 12 11 15
+
+	return true;
 }
 
 int main() {
+	bool play = true;
 
 	int **board;
+	int **solvedBoard;
 
-	board = new int*[4];
-	for (size_t i = 0; i < 4; i++)
+	int boardWidth = 4;
+	int shuffleCount = 5;
+
+	bool shuffle = false;
+
+	cout << "Bourd Size?" << endl;
+
+	cin >> boardWidth;
+
+	board = new int*[boardWidth];
+	solvedBoard = new int*[boardWidth];
+	for (size_t i = 0; i < boardWidth; i++)
 	{
-		board[i] = new int[4];
+		board[i] = new int[boardWidth];
+		solvedBoard[i] = new int[boardWidth];
 	}
 
 	int num = 1;
 
 	//Creates board
-	for (size_t col = 0; col < 4; col++)
+	for (size_t col = 0; col < boardWidth; col++)
 	{
-		for (size_t row = 0; row < 4; row++)
+		for (size_t row = 0; row < boardWidth; row++)
 		{
-			if (num == 16) {
+			if (num == boardWidth * boardWidth) {
 				num = 0;
 			}
 			board[col][row] = num;
+			solvedBoard[col][row] = num;
 			num++;
 		}
 	}
 
 	cout << "Sliding Puzzle:" << endl;
 
-	printBoard(board);
+	printBoard(boardWidth, board);
 
-	shuffleBoard(board);
+	while (play) {
+		cout << "print, " << "shuffle, " << "solve, " << " or exit" << endl;
+		char userInput[8] = {};
+		cin >> userInput;
 
-	printBoard(board);
+		if (strcmp(userInput, "print") == 0) {
+			printBoard(boardWidth, board);
+		}
 
+		if(strcmp(userInput, "shuffle") == 0) {
+			cout << "shuffle count" << endl;
+			cin >> shuffleCount;
+			shuffleBoard(shuffleCount, boardWidth, board);
+			printBoard(boardWidth, board);
+			cout << "shuffled" << endl;
+		}
+		
+		if (strcmp(userInput, "solve") == 0) {
+			while (!solveBoard(boardWidth, board, solvedBoard)) {
+				cout << "solving" << endl;
+				printBoard(boardWidth, board);
+			}
+			cout << "solved board" << endl;
+		}
 
-	while (!solveBoard(board)) {
-		cout << "Solving" << endl;
-		printBoard(board);
+		if (strcmp(userInput, "exit") == 0) {
+			play = false;
+		}
 	}
-	cout << "Solved Board" << endl;
-	
-	for (size_t i = 0; i < 4; i++)
+
+	for (size_t i = 0; i < boardWidth; i++)
 	{
 		delete[] board[i];
 	}
