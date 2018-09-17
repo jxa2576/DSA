3bc3ee7ceb8cba7fe5fe5557c4705fc98de801a9
Jeb Atkinson <jxa2576@g.rit.edu>
2018-09-17 01:40:15 -0400

    Kinda hill climb?

--- SlidingPuzzle/SlidingPuzzle/SlidingPuzzle.cpp
+++ SlidingPuzzle/SlidingPuzzle/SlidingPuzzle.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <time.h>
+#include <cmath>
 
 using namespace std;
 
 void printBoard(int** b) {
-	cout << "Sliding Puzzle:" << endl;
 	cout << "--------------" << endl;
 	for (size_t col = 0; col < 4; col++)
 	{
@@ -11,7 +12,7 @@ void printBoard(int** b) {
 
 		for (size_t row = 0; row < 4; row++)
 		{
-			if (b[col][row] != 0) {
+			if (b[col][row] != -1) {
 				cout << b[col][row];
 				cout << " ";
 				if (b[col][row] < 10) {
@@ -31,7 +32,8 @@ void printBoard(int** b) {
 void shuffleBoard(int** b) {
 	cout << "SHUFFLE" << endl;
 	int shuffleCount = 0;
-	while (shuffleCount < 5) {
+	srand(time(NULL));
+	while (shuffleCount < 15) {
 		for (size_t col = 0; col < 4; col++)
 		{
 			for (size_t row = 0; row < 4; row++)
@@ -76,6 +78,114 @@ void shuffleBoard(int** b) {
 	}
 }
 
+bool solveBoard(int** b) {
+	int solution[16] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0};
+
+	int solveCount = 0;
+	bool solved = false;
+	int colPos = 3;
+	int rowPos = 3;
+	int moves[4] = { 0,0,0,0 };
+
+	for (size_t col = 0; col < 4; col++)
+	{
+		for (size_t row = 0; row < 4; row++)
+		{
+			if (b[col][row] == solution[4 * col + row]) {
+				solveCount++;
+			}
+			else if(b[col][row] == 0){
+				colPos = col;
+				rowPos = row;
+				if (col - 1 != -1) {
+					//cout << "UP" << endl;
+					moves[0] = abs(b[col - 1][row] - solution[4 * (col) + row]);
+				}
+				else {
+					moves[0] = -1;
+				}
+
+				if (row - 1 != -1) {
+					//cout << "LEFT" << endl;
+					moves[1] = abs(b[col][row - 1] - solution[4 * col + (row)]);
+				}
+				else {
+					moves[1] = -1;
+				}
+
+				if (col + 1 != 4) {
+					//cout << "DOWN" << endl;
+					moves[2] = abs(b[col + 1][row] - solution[4 * (col) + row]);
+				}
+				else {
+					moves[2] = -1;
+				}
+
+				if (row + 1 != 4) {
+					//cout << "RIGHT" << endl;
+					moves[3] = abs(b[col][row + 1] - solution[4 * col + (row)]);
+				}
+				else {
+					moves[3] = -1;
+				}
+			}
+		}
+	}
+	if (solveCount == 16) {
+		solved = true;
+	}
+	else {
+		cout << "NOT SOLVED: " << colPos << " " << rowPos << endl;
+		bool stuck = false;
+		for (size_t i = 0; i < 4; i++)
+		{
+			cout << moves[i] << endl;
+		}
+		bool movement = false;
+		int spotToMove = 0;
+		while (!movement) {
+			for (size_t i = 0; i < 4; i++)
+			{
+				if (moves[i] == spotToMove && movement == false) {
+					movement = true;
+					switch (i)
+					{
+					case 0:
+						//cout << "UP" << endl;
+						b[colPos][rowPos] = b[colPos - 1][rowPos];
+						b[colPos - 1][rowPos] = 0;
+						break;
+					case 1:
+						//cout << "LEFT" << endl;
+						b[colPos][rowPos] = b[colPos][rowPos - 1];
+						b[colPos][rowPos - 1] = 0;
+						break;
+					case 2:
+						//cout << "DOWN" << endl;
+						b[colPos][rowPos] = b[colPos + 1][rowPos];
+						b[colPos + 1][rowPos] = 0;
+						break;
+					case 3:
+						//cout << "RIGHT" << endl;
+						b[colPos][rowPos] = b[colPos][rowPos + 1];
+						b[colPos][rowPos + 1] = 0;
+						break;
+					}
+				}
+			}
+			spotToMove++;
+		}
+	}
+
+	return solved;
+}
+
+bool recursiveSolve(int** b, int depth) {
+	depth++;
+	//check if solved
+	//foreach possible move, move, call function again, use for loop
+}
+
 int main() {
 
 	int **board;
@@ -101,13 +211,25 @@ int main() {
 		}
 	}
 
+	cout << "Sliding Puzzle:" << endl;
+
 	printBoard(board);
 
 	shuffleBoard(board);
 
 	printBoard(board);
 
-	cin.get();
+
+	while (!solveBoard(board)) {
+		cout << "Solving" << endl;
+		printBoard(board);
+	}
+	cout << "Solved Board" << endl;
+	
+	for (size_t i = 0; i < 4; i++)
+	{
+		delete[] board[i];
+	}
 
 	delete[] board;
 
